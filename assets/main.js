f25e987def76d1c439b96b9ae09f482aee232305
realabdullah <abdulodesanmi@gmail.com>
2022-06-11 22:31:50 +0100

    js

--- assets/main.js
+++ assets/main.js
@@ -1,22 +1,31 @@
-document.getElementById('submit').onclick = function () {
-  let bill = document.getElementById('bill').value
-  let people = document.getElementById('people').value
+document.getElementById('people').addEventListener('keyup', function (event) {
+  if (event.code === 'Enter') {
+    event.preventDefault()
+    let bill = document.getElementById('bill').value
+    let people = document.getElementById('people').value
 
-  let tipPercents = document.getElementsByName('tip-percent')
-  
-  var selectedPercent = Array.from(tipPercents).find(percent => percent.checked)
+    let tipPercents = document.getElementsByName('tip-percent')
 
-  let tip = 1 + (selectedPercent.value / 100)
-  let totalTip = bill * tip
+    var selectedPercent = Array.from(tipPercents).find(percent => percent.checked)
 
-  let totalPerPerson = totalTip / people
+    let tip = 1 + (selectedPercent.value / 100)
+    let totalTip = bill * tip
 
-  let totalTipPerPerson = totalTip - bill
+    let totalPerPerson = totalTip / people
 
-  let eachPersonTip = totalTipPerPerson / people
-  console.log(eachPersonTip)
+    let totalTipPerPerson = totalTip - bill
 
-  document.querySelector('.tip__amount').innerHTML = '$' + eachPersonTip.toFixed(2)
+    let eachPersonTip = totalTipPerPerson / people
 
-  document.querySelector('.total__amount').innerHTML = '$' + totalPerPerson.toFixed(2)
+    document.querySelector('.tip__amount').innerHTML = '$' + eachPersonTip.toFixed(2)
+
+    document.querySelector('.total__amount').innerHTML = '$' + totalPerPerson.toFixed(2)
+  }
+})
+
+document.getElementById('reset').onclick = function () {
+  document.querySelector('#bill').value = ''
+  document.querySelector('#people').value = ''
+  document.querySelector('.tip__amount').innerHTML = '$0.00'
+  document.querySelector('.total__amount').innerHTML = '$0.00'
 }
